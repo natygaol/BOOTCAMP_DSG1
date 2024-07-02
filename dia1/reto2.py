bb8d0fab3b4d96c63f9546f3fc1d2626c451c2cd
Nataly Garcia <naty_garcioli@hotmail.com>
2024-07-01 22:48:37 -0500

    added list comprehensions

--- dia1/reto2.py
+++ dia1/reto2.py
@@ -9,3 +9,9 @@ def get_evens(numbers):
 numbers = [-1, 70, 3, 76, 99, 2, 7, 20, 66, 550]
 evens = get_evens(numbers)
 print(evens)
+
+
+#list comprehensions
+
+pairs = [number for number in numbers if number % 2 == 0]
+print(pairs)

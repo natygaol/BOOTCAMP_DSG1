859ac2d3d7cf0b8d499a28118ca7cb8ce8f7826d
Nataly Garcia <naty_garcioli@hotmail.com>
2024-07-01 22:35:25 -0500

    adds get_evens method

---
+++ dia1/reto2.py
@@ -0,0 +1,11 @@
+def get_evens(numbers):
+  evens = []
+  for number in numbers:
+    if number % 2 == 0:
+      evens.append(number)
+  return evens
+
+
+numbers = [-1, 70, 3, 76, 99, 2, 7, 20, 66, 550]
+evens = get_evens(numbers)
+print(evens)

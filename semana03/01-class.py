ee1d72afc001de1f2e89873e2119a309317ed654
Nataly Garcia <naty_garcioli@hotmail.com>
2024-07-08 19:40:04 -0500

    first method

---
+++ semana03/01-class.py
@@ -0,0 +1,13 @@
+class Automovil:
+  def __init__(self, aa, pl, col, mar):
+    self.anio = aa
+    self.placa = pl
+    self.color = col
+    self.marca = mar
+
+  def encender(self):
+    print('encender ' + self.marca)
+
+#objetos
+vw = Automovil(1970, 'CH1234', 'Amarillo', 'Volkswagen')
+vw.encender()

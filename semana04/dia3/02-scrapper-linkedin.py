3643a9340b1c0ec33c14e11777bebf3c17d5ce29
Nataly Garcia <naty_garcioli@hotmail.com>
2024-07-19 22:06:47 -0500

    adds new way of importing tags

--- semana04/dia3/02-scrapper-linkedin.py
+++ semana04/dia3/02-scrapper-linkedin.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import mysql.connector
 
 LINKEDIN_URL = "https://www.linkedin.com/jobs/search/?geoId=102927786&keywords="
 
@@ -14,16 +15,17 @@ def get_offers(skill):
 
         ul_offers = html.find('ul',{'class':'jobs-search__results-list'})
         li_offers = ul_offers.find_all('li')
-        #print(li_offers)
         
         for offer in li_offers:
             offer_title = offer.find('h3',{'class':'base-search-card__title'})
             offer_location = offer.find('span',{'class':'job-search-card__location'})
-            offer_company = offer.find('a')
-            offer_url = offer_company['href']
+            offer_url = offer.find('a')['href'].strip()
+            offer_company = offer.find('a',{'class':'hidden-nested-link'})
+            company = offer_company.get_text().strip() if offer_company else 'N/A'
+
             print(f'titulo : {offer_title.get_text().strip()}')
             print(f'ubicacion : {offer_location.get_text().strip()}')
-            print(f'empresa : {offer_company.get_text().strip()}')
+            print(f'empresa : {company}')
             print(f'url:{offer_url}')
     else:
         print(f"error : {url.status_code}")

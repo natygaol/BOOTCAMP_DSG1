4f83b3f30c589cb70f1522f059fee660199e83e2
Nataly Garcia <naty_garcioli@hotmail.com>
2024-07-19 22:27:42 -0500

    adds offer_list

--- semana04/dia3/02-scrapper-linkedin.py
+++ semana04/dia3/02-scrapper-linkedin.py
@@ -15,24 +15,34 @@ def get_offers(skill):
 
         ul_offers = html.find('ul',{'class':'jobs-search__results-list'})
         li_offers = ul_offers.find_all('li')
+
+        offer_list = []
         
         for offer in li_offers:
             offer_title = offer.find('h3',{'class':'base-search-card__title'})
             offer_location = offer.find('span',{'class':'job-search-card__location'})
-            offer_url = offer.find('a')['href'].strip()
+            offer_url = offer.find('a')
             offer_company = offer.find('a',{'class':'hidden-nested-link'})
-            company = offer_company.get_text().strip() if offer_company else 'N/A'
             offer_date = offer.find('time',{'class':'job-search-card__listdate'})
-            offer_date_value = offer_date['datetime'].strip() if offer_date else None
+            
+            title = offer_title.get_text().strip() if offer_title else ''
+            location = offer_location.get_text().strip() if offer_location else ''
+            url_value = offer_url['href'].strip() if offer_url else ''
+            company = offer_company.get_text().strip() if offer_company else ''
+            date_value = offer_date['datetime'].strip() if offer_date else None
 
-            print(f'titulo : {offer_title.get_text().strip()}')
+            """print(f'titulo : {offer_title.get_text().strip()}')
             print(f'ubicacion : {offer_location.get_text().strip()}')
             print(f'empresa : {company}')
             print(f'url:{offer_url}')
-            print(f'fecha : {offer_date_value}')
+            print(f'fecha : {offer_date_value}')"""
+            offer_list.append((title,location,company,date_value,url_value))
+            
+        return offer_list
     else:
         print(f"error : {url.status_code}")
 
 if __name__ == '__main__':
     skill = input('ingrese skill a buscar : ')
-    get_offers(skill)
+    offer_list = get_offers(skill)
+    print(offer_list)
